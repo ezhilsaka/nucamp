291ae00ad0521461086b57866adcfc8379f84517
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-06-08 12:59:42 -0400

    week4 advancedsql exercises

--- Python/2-SQL/common_files/queries.sql
+++ Python/2-SQL/common_files/queries.sql
@@ -322,3 +322,70 @@ frame_height,
 frame_width * frame_height AS frame_area
 FROM frames
 GROUP BY frame_width, frame_height, frame_area;
+
+--JSON in postgres
+
+SELECT jsonb_pretty(info) AS formatted_info
+FROM moma_artists LIMIT 50
+
+SELECT
+info -> 'display_name' AS name,
+info -> 'nationality' as nationality
+FROM moma_artists
+ORDER BY id
+LIMIT 50;
+
+--Different ways of inserting data in JSONB object in postgres
+
+INSERT INTO moma_artists (info) VALUES (
+    json_object('{display_name, Ablade Glover, nationality, Ghanaian}')
+);
+
+INSERT INTO moma_artists (info) VALUES (
+    json_object('{{display_name, Ablade Glover}, {nationality, Ghanaian}}')
+);
+
+INSERT INTO moma_artists (info) VALUES (
+    json_object('{display_name, nationality}', '{Ablade Glover, Ghanaian}')
+);
+
+--Triggers Example
+
+CREATE FUNCTION log_new_employee() RETURNS trigger AS $$
+    BEGIN
+        INSERT INTO employees_log (description, employee_id) VALUES (
+            'Employee created.',
+            NEW.id
+        );
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER log_new_employee AFTER INSERT ON employees
+    FOR EACH ROW EXECUTE FUNCTION log_new_employee();
+
+INSERT INTO employees (salary, name) values (55000, 'Alice');
+INSERT INTO employees (salary, name) values (66000, 'Bob');
+
+SELECT e.*, el.description, el.created_at
+FROM employees_log el
+JOIN employees e ON el.employee_id = e.id;
+
+CREATE FUNCTION log_salary_update() RETURNS trigger AS $$
+    BEGIN
+        INSERT INTO employees_log (description, employee_id) VALUES (
+            'Salary updated from '||OLD.salary||' to '||NEW.salary,
+            NEW.id
+        );
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER log_salary_update AFTER UPDATE OF salary ON employees
+    FOR EACH ROW EXECUTE FUNCTION log_salary_update();
+
+UPDATE employees SET salary = 80000 WHERE name = 'Alice';
+
+SELECT e.*, el.description, el.created_at
+FROM employees_log el
+JOIN employees e ON el.employee_id = e.id;
